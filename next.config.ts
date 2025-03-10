dbcdacf39ca2eeea60c63d0a1c16e7fae15bc9f7
Luka Budík <sheffikthegod@gmail.com>
2025-03-10 18:30:04 +0100

    fix: Cloudflare proxy headers

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Required for Cloudflare proxy to work properly
+  headers: async () => {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Forwarded-Proto',
+            value: 'https',
+          },
+        ],
+      },
+    ];
+  },
   /* config options here */
 };
 
